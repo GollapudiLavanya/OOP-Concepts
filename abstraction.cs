64ad37036b79c88151085ed7067bec5065e918c1
GollapudiLavanya <86302821+GollapudiLavanya@users.noreply.github.com>
2021-07-08 10:14:02 +0530

    Update abstraction.cs

--- abstraction.cs
+++ abstraction.cs
@@ -1,28 +1,36 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace oopsconcepts
+using System;
+  
+namespace Demoabstraction
 {
-
-    public class Laptop
-    {
-        private string brand;
-        private string model;
-        public string Brand
-        {
-            get { return brand; }
-            set { brand = value; }
-        }
-        public string Model
-        {
-            get { return model; }
-            set { model = value; }
-        }
-        public void LaptopDetails()
-        {
-            Console.WriteLine("Brand: " + Brand);
-            Console.WriteLine("Model: " + Model);
-        }
-    }
+      
+// abstract class
+abstract class Shape 
+{
+  
+    // abstract method
+    public abstract int area();
 }
+  
+// square class inherting
+// the Shape class
+class Square : Shape 
+{
+  
+    // private data member
+    private int side;
+  
+    // method of  square class
+    public Square(int x = 0)
+    {
+        side = x;
+    }
+      
+    // overriding of the abstract method of Shape
+    // class using the override keyword
+    public override int area()
+    {
+        Console.Write("Area of Square: ");
+        return (side * side);
+     }
+     }
+ }
