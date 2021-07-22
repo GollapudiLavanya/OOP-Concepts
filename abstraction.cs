ca304ba85f907cd883f1b843aecb821bf3fccfbf
GollapudiLavanya <86302821+GollapudiLavanya@users.noreply.github.com>
2021-07-22 12:42:31 +0530

    Update abstraction.cs

--- abstraction.cs
+++ abstraction.cs
@@ -2,35 +2,19 @@ using System;
   
 namespace Demoabstraction
 {
-      
-// abstract class
-abstract class Shape 
-{
-  
-    // abstract method
-    public abstract int area();
-}
-  
-// square class inherting
-// the Shape class
-class Square : Shape 
-{
-  
-    // private data member
-    private int side;
-  
-    // method of  square class
-    public Square(int x = 0)
-    {
-        side = x;
-    }
-      
-    // overriding of the abstract method of Shape
-    // class using the override keyword
-    public override int area()
-    {
-        Console.Write("Area of Square: ");
-        return (side * side);
-     }
-     }
+   abstract class animal  
+    {  
+        public abstract void eat();  
+        public void sound()  
+        {  
+            Console.WriteLine("dog can sound");  
+        }  
+    }  
+    class dog : animal  
+    {  
+        public override void eat()  
+        {  
+            Console.WriteLine("dog can eat");  
+        }  
+    }  
  }
